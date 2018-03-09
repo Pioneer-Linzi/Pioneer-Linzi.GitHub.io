3f7b60fdf1658e4886ec8b04cb4c6f7fe9be9095
linzi <linzi4235@gmail.com>
2018-03-09 16:44:39 +0800

    gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,19 @@
 var gulp = require('gulp');
-var del = require('del');
+var del = require('del')
 
 gulp.task('clean',function(){
     return del('./dist/');
 });
 
 gulp.task('blog',function(){
-    gulp.src('./src/blog/dist/index.html').pipe(gulp.dest('./dist/'));
+    gulp.src('./src/blog/dist/**/*.*').pipe(gulp.dest('./dist/'));
 });
 
+gulp.task('learnDemo',function(){
+    gulp.src('./src/reactDemo/build/**/*.*').pipe(gulp.dest('./dist/reactDemo/'));
+});
 gulp.task('default',['clean'], function() {
-    return gulp.start('blog');
+    return gulp.start('blog','learnDemo');
 });
 
 
