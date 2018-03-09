8c82383388f890be06ae9f20fcd27fb9229abc0a
linzi <linzi4235@gmail.com>
2018-03-09 16:23:01 +0800

    gulp success blog

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,15 @@
 var gulp = require('gulp');
+var del = require('del');
 
 gulp.task('clean',function(){
     return del('./dist/');
 });
 
 gulp.task('blog',function(){
-    gulp.src('./src/blog/dist/index.html').pipe(gulp.dest('./dist/index.html'));
+    gulp.src('./src/blog/dist/index.html').pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('default', function() {
+gulp.task('default',['clean'], function() {
     return gulp.start('blog');
 });
 
