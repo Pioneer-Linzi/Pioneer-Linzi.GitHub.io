129c935f4ad9eabf5354daf545a79f0689544132
linzi <linzi4235@gmail.com>
2018-03-14 14:55:53 +0800

    finish

---
+++ src/reactdemo/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Index from './pages/Index.jsx';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <Index></Index>
+      </div>
+    );
+  }
+}
+
+export default App;
