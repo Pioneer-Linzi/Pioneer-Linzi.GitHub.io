31c7be2a46756f47e86253fd5d76a0b03a45b559
linzi <linzi4235@gmail.com>
2018-03-24 18:19:44 +0800

    change some sth

---
+++ src/blog/src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+	<div id="Header">
+		<a href="./page/reactDemo.html">reactDemo</a>
+		<img src="./assets/logo.png">
+		<router-view/>
+	</div>
+</template>
+<script>
+	export default {
+		name: ''
+	}
+</script>
+<style lang="less">
+@import "ui/test.less";
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+</style>
