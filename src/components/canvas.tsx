29daa9eb6a060f35a9a774d3a7c5e3276ea91765
zhaolinlin <linzi4235@gmail.com>
2018-07-21 09:35:15 +0800

    add Tanks and panel controller

--- src/components/canvas.tsx
+++ src/components/canvas.tsx
@@ -4,7 +4,6 @@ import './canvas.scss'
 class CanvasPanel extends React.Component{
     constructor(props:any){
         super(props);
-        // this.getCanvas();
     }
 
     componentDidMount(){
---
+++ src/game/tank/Panel.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+
+
+class Panel extends React.PureComponent{
+    constructor(){
+        
+    }
+	
+
+}
