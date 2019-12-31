f493f0c08c2220827c8f07ea660c921c7647b8ae
赵林林 <linlin.zhao@ximalaya.com>
2019-12-31 10:39:16 +0800

    animotion

--- src/pages/css/index.tsx
+++ src/pages/css/index.tsx
@@ -10,6 +10,27 @@ export default function (props: {}) {
                 <div className="dot"></div>
                 <div className="dot"></div>
             </div>
+            <LandInText text='learn someThing'/>
+        </div>
+    )
+}
+
+function LandInText(props:{text: string}){
+    const {text} = props;
+    
+    return (
+        <div className="LandInText">
+            <p className="landIn">
+                {
+                    text.split('').map(function(item,index){
+                        console.log(index)
+                        const style = {
+                           'animationDelay': `${index*0.5}s`
+                        }
+                        return <span className='LandInTextAni' style={style} key={Math.random()}>{item}</span>
+                    })
+                }
+            </p>
         </div>
     )
 }
